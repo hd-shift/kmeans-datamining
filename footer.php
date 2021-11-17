da344a2b55f0fd455c49aa61a75b437e5fde72fe
hd-shift <nurulhuda.unhasy@gmail.com>
2021-11-17 11:25:03 +0700

    menambah footer dan link js terpisah

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="proses.php" class="nav-link px-2 text-muted">Proses</a></li>
+        <li class="nav-item"><a href="output.php" class="nav-link px-2 text-muted">Output</a></li>
+      </ul>
+      <p class="text-center text-muted">&copy; 2021 UAS DATA MINING | ALGORITMA K-MEANS | KELOMPOK 1</p>
+    </footer>
+  </div>
