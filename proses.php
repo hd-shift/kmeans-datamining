d2e47c25919481351135bc9a7a66c2236dc21956
hd-shift <nurulhuda.unhasy@gmail.com>
2021-11-17 11:24:32 +0700

    membagi beberapa bagian dan menambha footer

--- proses.php
+++ proses.php
@@ -151,15 +151,12 @@
     </div>
     
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-  </script>
-
-  <!-- Jquery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <!-- Data Tables -->
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js">
-  </script>
+<?php 
+    include 'footer.php';
+  ?>
+  <?php 
+    include 'js.php';
+  ?>
   <script>
     $(document).ready(function() {
       $('#table-cluster').DataTable();
